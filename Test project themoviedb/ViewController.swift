de8192e7b0c159d1e3268e2a65ca6344b2ff4df8
Suleman Ali <suleman.ali303@gmail.com>
2021-10-14 22:55:19 +0500

    Initial Commit

---
+++ Test project themoviedb/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test project themoviedb
+//
+//  Created by Suleman on 14/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Test project themoviedbTests/Test_project_themoviedbTests.swift
@@ -0,0 +1,33 @@
+//
+//  Test_project_themoviedbTests.swift
+//  Test project themoviedbTests
+//
+//  Created by Suleman on 14/10/2021.
+//
+
+import XCTest
+@testable import Test_project_themoviedb
+
+class Test_project_themoviedbTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
